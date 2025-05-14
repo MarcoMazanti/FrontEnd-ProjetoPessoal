01dbb82ac23569c371b76582a6a9084c1336f2fc
MarcoMazanti <marcauremaza@gmail.com>
2025-05-14 14:39:22 -0300

    first commit

---
+++ Models/Usuario.js
@@ -0,0 +1,9 @@
+export class Usuario{
+    constructor(id, nome, email, senha, imagem){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.imagem = imagem;
+    }
+}
